f260f6c247b25150e56a9b1aa1db0dbaaaece8f9
Rogério da Silva Yokomizo <me@ro.ger.io>
2016-02-29 23:03:37 +0100

    Initial Commit

---
+++ MosquitoReport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MosquitoReport
+//
+//  Created by Rogério Yokomizo on 2/29/16.
+//  Copyright © 2016 Coding for People. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
